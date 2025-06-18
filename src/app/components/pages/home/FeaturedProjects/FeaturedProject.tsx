b0c73c08708985b933e7e7138bd4d2d19268bb57
tinanvo <tinanhivo@gmail.com>
2025-06-18 17:08:10 +1000

    - Add more featured projects - Add Featured Projects section animation

--- src/app/components/pages/home/FeaturedProjects/FeaturedProject.tsx
+++ src/app/components/pages/home/FeaturedProjects/FeaturedProject.tsx
@@ -4,20 +4,39 @@ import SectionSpacing from '../../../SectionSpacing/SectionSpacing';
 import TextBubble from '../../../TextBubble/TextBubble';
 import './FeaturedProject.css';
 
-export default function FeaturedProject() {
+type Project = {
+  title: string;
+  skills: string[];
+  img: string;
+  bgColor: string;
+  textColor: string;
+};
+
+type Props = {
+  project: Project;
+};
+
+export default function FeaturedProject({ project }: Props) {
   return (
-    <div className='featured_project_wrap'>
+    <div
+      className='featured_project_wrap'
+      style={{ backgroundColor: project.bgColor, color: project.textColor }}
+    >
       <div className='featured_project_contain flex flex-col items-center justify-center'>
         <SectionSpacing variant='small' />
         <div className='featured_project_layout u-container flex flex-col items-start justify-between'>
-          <h2 className='featured_project_heading u-text-style-h2'>jetia</h2>
-          <div className='project_skills_wrap'>
-            <TextBubble>Skills</TextBubble>
+          <h2 className='featured_project_heading u-text-style-h2'>
+            {project.title}
+          </h2>
+          <div className='featured_project_skills_list flex flex-row'>
+            {project.skills.map((skill, i) => {
+              return <TextBubble key={i}>{skill}</TextBubble>;
+            })}
           </div>
           <div className='featured_project_image_wrap flex flex-col items-center justify-center'>
             <div className='featured_project_image'>
               <Image
-                src='/images/jetia.png'
+                src={project.img}
                 alt='jetia landing page'
                 fill
                 sizes='75vw'
--- src/app/components/pages/home/FeaturedProjects/FeaturedProjects.tsx
+++ src/app/components/pages/home/FeaturedProjects/FeaturedProjects.tsx
@@ -1,10 +1,79 @@
+'use client';
+
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useEffect, useRef } from 'react';
+
 import FeaturedProject from './FeaturedProject';
 import './FeaturedProjects.css';
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function FeaturedProjects() {
+  const projectListRef = useRef<HTMLDivElement>(null);
+
+  const PROJECTS = [
+    {
+      title: 'jetia',
+      skills: ['React', 'Web Development'],
+      img: '/images/jetia-1.png',
+      bgColor: '#df7496',
+      textColor: 'var( --color--dark)',
+    },
+    {
+      title: 'PokéBattles',
+      skills: ['React', 'Web Development'],
+      img: '/images/pokemon-1.png',
+      bgColor: '#ffe030',
+      textColor: 'var( --color--dark)',
+    },
+    {
+      title: 'MNTN',
+      skills: ['Web Development', 'Webflow'],
+      img: '/images/mntn-1.png',
+      bgColor: '#0b1d26',
+      textColor: 'var(--color--light)',
+    },
+  ];
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const section = projectListRef.current;
+      if (!section) return;
+
+      const projectItems = section.querySelectorAll('.featured_project_wrap');
+
+      const tl = gsap.timeline({
+        paused: true,
+        scrollTrigger: {
+          trigger: section,
+          start: 'top top',
+          end: '+=300%',
+          scrub: true,
+          pin: true,
+        },
+      });
+
+      projectItems.forEach((item, index) => {
+        if (index > 0) {
+          tl.from(item, { yPercent: 100 });
+          tl.to({}, { duration: 0.5 }); // Spacer between animations
+        }
+      });
+    }, projectListRef);
+
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <section className='featured_projects_wrap'>
-      <FeaturedProject />
+    <section ref={projectListRef} className='featured_projects_wrap'>
+      {PROJECTS.map((project, i) => {
+        return (
+          <section key={i} className='featured_projects_item_wrap'>
+            <FeaturedProject project={project} />
+          </section>
+        );
+      })}
     </section>
   );
 }
